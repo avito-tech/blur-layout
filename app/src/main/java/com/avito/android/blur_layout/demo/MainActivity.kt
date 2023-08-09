f37a7ea929584683b1b7b2248873fd2cc77fdeaa
aakorolev <aakorolev@avito.ru>
2023-08-09 18:39:19 +0300

    added demo page

--- app/src/main/java/com/avito/android/blur_layout/demo/MainActivity.kt
+++ app/src/main/java/com/avito/android/blur_layout/demo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.avito.android.blur_layout.demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.avito.android.blur_layout.BlurLayout
+import com.google.android.renderscript.Toolkit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BlurLayout.onApplyBlur = { bitmap, blurRadius ->
+            Toolkit.blur(inputBitmap = bitmap, radius = blurRadius)
+        }
     }
 }
