000e82604c8f9ee5cc299b1a70fa795b907e2018
aakorolev <aakorolev@avito.ru>
2023-08-10 13:14:49 +0300

    added better examples

--- app/src/main/java/com/avito/android/blur_layout/demo/MainActivity.kt
+++ app/src/main/java/com/avito/android/blur_layout/demo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.avito.android.blur_layout.demo
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.avito.android.blur_layout.BlurLayout
 import com.google.android.renderscript.Toolkit
 
@@ -13,5 +15,28 @@ class MainActivity : AppCompatActivity() {
         BlurLayout.onApplyBlur = { bitmap, blurRadius ->
             Toolkit.blur(inputBitmap = bitmap, radius = blurRadius)
         }
+
+        val buttons = mutableListOf<View>()
+
+        fun hideButtons() {
+            buttons.forEach { it.isVisible = false }
+        }
+
+        val exampleRocks = findViewById<View>(R.id.example_rocks)
+        val exampleFruits = findViewById<View>(R.id.example_fruits)
+
+        findViewById<View>(R.id.button_rocks)
+            .apply(buttons::add)
+            .setOnClickListener {
+                hideButtons()
+                exampleRocks.isVisible = true
+            }
+
+        findViewById<View>(R.id.button_fruits)
+            .apply(buttons::add)
+            .setOnClickListener {
+                hideButtons()
+                exampleFruits.isVisible = true
+            }
     }
 }
